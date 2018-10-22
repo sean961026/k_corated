90699e9e5fe3dd63838b6e5de5cdb187cade8d4d
sean <sean961026@gmail.com>
2018-10-22 14:52:28 -0400

    new file

--- jester.py
+++ jester.py
@@ -2,7 +2,7 @@ import numpy as np
 from rs import unknown_rating, load, dump
 
 
-def create_ratings(user_size, type, filename):
+def create_ratings(user_size, filename):
     ratings = np.zeros(shape=(user_size, 150))
     with open('jester_dataset_2/jester_ratings.dat') as file:
         lines = file.readlines()
@@ -10,9 +10,9 @@ def create_ratings(user_size, type, filename):
             user_id, item_id, rating = line.split('\t\t')
             if int(user_id) >= user_size:
                 break
-            ratings[int(user_id) - 1, int(item_id) - 1] = type(rating)
+            ratings[int(user_id) - 1, int(item_id) - 1] = round(float(rating) + 21)
     dump(filename, ratings)
     return ratings
 
 
-create_ratings(63978, float, 'test')
+create_ratings(63978, 'test')
