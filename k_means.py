58078fdf4138dbb8d97a93450c9d5dcfe02f2c16
sean <sean961026@gmail.com>
2018-11-14 16:43:30 -0500

    new k means

--- k_means.py
+++ k_means.py
@@ -2,13 +2,49 @@ import random
 import numpy as np
 import logging
 import argparse
-from rs import get_ratings_name_from_dataset, load
-from k_corated_by import sort
+from rs import get_ratings_name_from_dataset, load, supp_user
+from functools import cmp_to_key
+
+
+def set_cmp(user1, user2):
+    items_1 = supp_user(user1)
+    items_2 = supp_user(user2)
+    while len(items_1) != 0:
+        if min(items_1) < min(items_2):
+            return -1
+        elif min(items_1) > min(items_2):
+            return 1
+        else:
+            min_value = min(items_1)
+            items_1.remove(min_value)
+            items_2.remove(min_value)
+    return 0
+
+
+def mycmp(user1, user2):
+    if len(supp_user(user1)) < len(supp_user(user2)):
+        return -1
+    elif len(supp_user(user1)) > len(supp_user(user2)):
+        return 1
+    else:
+        return set_cmp(user1, user2)
 
 
 def get_sort_index(ratings):
     sorted_one = sort(ratings)
     return list(sorted_one[:, -1])
+
+
+def sort(ratings):
+    user_size = ratings.shape[0]
+    item_size = ratings.shape[1]
+    ratings = np.insert(ratings, item_size, [i + 1 for i in range(user_size)], 1)
+    temp = [ratings[i, :] for i in range(user_size)]
+    temp.sort(key=cmp_to_key(mycmp))
+    ret = np.zeros(shape=(user_size, item_size + 1))
+    for i in range(user_size):
+        ret[i, :] = temp[i].copy()
+    return ret
 
 
 def get_initial_seeds(original_ratings, size, mode):
