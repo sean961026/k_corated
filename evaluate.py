f177ae9bd67dc0ff88638d269e13b6f767a31d01
sean <sean961026@gmail.com>
2018-11-23 14:59:51 -0500

    new web

--- evaluate.py
+++ evaluate.py
@@ -8,6 +8,34 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 
 original_ratings = None
+adapter_type = None
+
+
+def adapter(num, type=0):
+    def fix(data):
+        if data < 1:
+            return 1
+        if data > 5:
+            return 5
+        return data
+
+    if type == 0:
+        return fix(int(num))
+    elif type == 1:
+        return fix(round(num))
+    elif type == 2:
+        if num <= 1.3:
+            return 1
+        elif num > 1.3 and num <= 2.6:
+            return 2
+        elif num > 2.6 and num <= 3.6:
+            return 3
+        elif num > 3.6 and num <= 4.2:
+            return 4
+        else:
+            return 5
+    else:
+        raise ValueError
 
 
 def RMSE(dataset, web, neighbor_fun, neighbor_para):
@@ -24,6 +52,7 @@ def RMSE(dataset, web, neighbor_fun, neighbor_para):
         test_item = int(record[1] - 1)
         test_rating = record[2]
         predicted_rating, des = pd_rating(original_ratings, test_user, test_item, web, neighbor_fun, neighbor_para)
+        predicted_rating = adapter(predicted_rating, adapter_type)
         count[des]['num'] += 1
         count[des]['ratings'][predicted_rating - 1] += 1
         count[des]['diff'][abs(int(predicted_rating - test_rating))] += 1
@@ -60,12 +89,14 @@ def main():
     parser.add_argument('-s', '--suffix')
     parser.add_argument('-t', '--threshold')
     parser.add_argument('--top', type=int)
+    parser.add_argument('-a', '--adapter', type=int)
     args = parser.parse_args()
     data_set = args.dataset
     web_name = args.web
     top = args.top
     threshold = args.threshold
     suffix = args.suffix
+    adapter_index = args.adapter
     init(data_set)
 
     def rmse(webname):
@@ -78,11 +109,28 @@ def main():
             return count
 
     if web_name != 'all':
-        count = rmse(web_name)
-        logging.info(count)
+        if adapter_index == None:
+            temp_tops = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
+            plt.figure()
+            plt.xlabel('top')
+            plt.ylabel('RMSE')
+            for adapter_index in [0, 1, 2]:
+                y = []
+                global adapter_type
+                adapter_type = adapter_index
+                for temp_top in temp_tops:
+                    top = temp_top
+                    count = rmse(web_name)
+                    y.append(count['RMSE'])
+                plt.plot(temp_tops, y, marker='*', label='adapter=%s' % adapter_index)
+            plt.legend()
+            plt.savefig('RMSE-evaluation.jpg')
+        else:
+            count = rmse(web_name)
+            logging.info(count)
     else:
         web_names = get_all_web_files(suffix)
-        temp_tops = [5, 10, 15, 20, 25, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
+        temp_tops = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
         plt.figure()
         plt.xlabel('top')
         plt.ylabel('RMSE')
--- rs.py
+++ rs.py
@@ -272,19 +272,7 @@ def pd_rating(original_ratings, user_id, item_id, web, neighbor_fun, neighbor_pa
         predicted_rating = user_mean
         des = 'exception'
 
-    def trans(num):
-        if num <= 1.3:
-            return 1
-        elif num > 1.3 and num <= 2.6:
-            return 2
-        elif num > 2.6 and num <= 3.6:
-            return 3
-        elif num > 3.6 and num <= 4.2:
-            return 4
-        else:
-            return 5
-
-    return trans(predicted_rating), des
+    return predicted_rating, des
 
 
 def get_all_web_files(suffix=None):
